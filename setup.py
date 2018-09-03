c3f6df35d36b7319c971876819fb092247a7aae4
Moksh Jain <mokshjn00@gmail.com>
2018-09-03 21:16:00 +0530

    add setup file

---
+++ setup.py
@@ -0,0 +1,41 @@
+from __future__ import absolute_import, print_function
+
+import io
+import re
+from os.path import dirname
+from os.path import join
+
+from setuptools import setup, find_packages
+
+
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ).read()
+
+
+setup(
+    name='gcforest',
+    url="https://github.com/MJ10/gcForest",
+    author='',
+    author_email='',
+    classifiers=[
+        'Development Status :: 1',
+
+        'Intended Audience :: Data scientists',
+        'Topic :: Deep Learning :: Deep Forests',
+
+        'Programming Language :: Python 2.7 :: Python 3.3+',
+    ],
+    keywords='random forest :: deep learning',
+    version='1.1.1',
+    license='MIT',
+    description='This is a simple implementation of the deep forest method',
+    long_description=re.compile('^.. start-badges.*^.. end-badges',
+                                re.M | re.S).sub('', read('README.md')),
+    packages=["gcforest"],
+    package_dir={"gcforest": "lib/gcforest"},
+    include_package_data=True,
+    zip_safe=True,
+)
